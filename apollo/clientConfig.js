4dc1b8c16d64c03940acf4e6c0d325d20d6cac98
srino <seenivasan9559@gmail.com>
2021-04-23 00:15:38 +0530

    chnage db

--- apollo/clientConfig.js
+++ apollo/clientConfig.js
@@ -2,11 +2,11 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 export default function(context) {
   return {
     httpLinkOptions: {
-      uri: 'http://localhost:8080/v1/graphql',
+      uri: 'https://hasurasetups.herokuapp.com/v1/graphql',
       credentials: 'same-origin'
     },
     cache: new InMemoryCache(),
-    wsEndpoint: 'ws://localhost:8080/v1/graphql',
+    wsEndpoint: 'ws://hasurasetups.herokuapp.com/v1/graphql',
     context
   }
 }
